50a1277b753f28810f0d22108e85824eca0070d6
Chris Rabl <rabl.christopher@gmail.com>
2015-10-17 15:01:44 -0600

    Added basic jquery scaffold

---
+++ app.js
@@ -0,0 +1,40 @@
+
+
+var tasks = [{
+  id: 0,
+  title: 'First task'
+}, {
+  id: 1,
+  title: 'Second task'
+}, {
+  id: 2,
+  title: 'Third task'
+}];
+
+
+function renderTaskList() {
+
+  var task_list = $('#task-list');
+
+  for (var i = 0; i < tasks.length; i++) {
+    var task = tasks[i];
+    var task_button = ' <button id="remove-' + task.id + '">x</button>';
+
+    task_list.append('<li id="' + task.id + '">' + task.title + task_button + '</li>');
+  }
+
+}
+
+function addTask(title) {
+  var next_id = tasks.length;
+
+  tasks.push({
+    id: next_id,
+    title: title
+  });
+}
+
+
+
+
+$(document).ready(renderTaskList);
